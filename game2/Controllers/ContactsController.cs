76f9e5448e5ab0c667d7ae2eee952e304cce084d
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-06-14 21:59:01 +0530

    fixed

--- game2/Controllers/ContactsController.cs
+++ game2/Controllers/ContactsController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using TestCrudApplication.Api.Dtos;
 
 namespace TestCrudApplication.Api.Controllers;
 
+[ApiController]
 [Route("Contacts")]
 public class ContactsController : ControllerBase
 {
@@ -9,5 +11,30 @@ public class ContactsController : ControllerBase
     public async Task<IActionResult> GetAll()
     {
         return Ok("this is working");
+    }
+
+    [HttpGet("{guid:guid}")]
+    public async Task<IActionResult> GetByUuid(Guid guid)
+    {
+        return Ok(guid);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateContact(CreateContactRequest request)
+    {
+        return Ok(request);
+    }
+
+
+    [HttpPut("{uuid:guid}")]
+    public async Task<IActionResult> UpdataContact(UpdateContactRequest request, Guid guid)
+    {
+        return Ok(new { request, guid });
+    }
+
+    [HttpDelete("{uuid:guid}")]
+    public async Task<IActionResult> DeleteContact(Guid guid)
+    {
+        return Ok(guid);
     }
 }
