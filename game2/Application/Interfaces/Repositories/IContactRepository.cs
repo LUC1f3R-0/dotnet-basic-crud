0d935d5caacc50ede52278949f266caaa0a8352a
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-06-17 21:48:38 +0530

    ficed

--- game2/Application/Interfaces/Repositories/IContactRepository.cs
+++ game2/Application/Interfaces/Repositories/IContactRepository.cs
@@ -1,4 +1,3 @@
-using TestCrudApplication.Application.Dtos.Contacts.Responses;
 using TestCrudApplication.Domain.Entities;
 
 namespace TestCrudApplication.Application.Interfaces;
@@ -6,16 +5,10 @@ namespace TestCrudApplication.Application.Interfaces;
 public interface IContactRepository
 {
     Task<List<Contact>> GetAllAsync();
-
     Task<Contact?> GetByUuidAsync(Guid uuid);
-
     Task<Contact> CreateAsync(Contact contact);
-
     Task<bool> ExistsByEmailAsync(string email);
-
     Task<bool> SearchByUuidAsync(Guid guid);
-
     Task<bool> UpdateAsync(Contact contact);
-
     Task<Contact> DeleteByUuidAsync(Contact contact);
 }
--- game2/Application/Interfaces/Services/IContactService.cs
+++ game2/Application/Interfaces/Services/IContactService.cs
@@ -5,12 +5,8 @@ namespace TestCrudApplication.Application.Interfaces;
 public interface IContactService
 {
     Task<List<ContactResponse>> GetAllAsync();
-
     Task<ContactResponse?> GetByUuidAsync(Guid uuid);
-
     Task<ContactResponse> CreateAsync(CreateContactRequest request);
-
     Task<ContactResponse?> UpdateByUuidAsync(Guid uuid, UpdateContactRequest request);
-
     Task<ContactResponse?> DeleteByUuidAsync(Guid uuid);
 }
